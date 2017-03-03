e8b7704e3069e03d2967d8c242b0a2cca8d3fe91
Luca pisani <luca.pisani@abstract.it>
2017-03-03 14:51:36 +0100

    remove logged in js

--- mockup/js/bundles/plone.js
+++ mockup/js/bundles/plone.js
@@ -1,29 +1,49 @@
-define([
+
+// Copyright (C) 2010 Plone Foundation
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation; either version 2 of the License.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+// more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program; if not, write to the Free Software Foundation, Inc., 51
+// Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+
+if (window.jQuery) {
+  define( 'jquery', [], function () {
+    'use strict';
+    return window.jQuery;
+  } );
+}
+
+require([
   'jquery',
   'pat-registry',
-  'pat-base',
+  'mockup-patterns-base',
 
   'mockup-patterns-select2',
   'mockup-patterns-pickadate',
-  'mockup-patterns-tinymce',
-
   'mockup-patterns-autotoc',
+  'mockup-patterns-cookietrigger',
   'mockup-patterns-formunloadalert',
   'mockup-patterns-preventdoublesubmit',
   'mockup-patterns-formautofocus',
+  'mockup-patterns-markspeciallinks',
   'mockup-patterns-modal',
-  'mockup-patterns-structure',
-  'mockup-patterns-textareamimetypeselector',
+  'mockup-patterns-livesearch',
+  'mockup-patterns-contentloader',
+  'mockup-patterns-moment',
   'bootstrap-dropdown',
-  'bootstrap-collapse'
+  'bootstrap-collapse',
+  'bootstrap-tooltip',
 ], function($, registry, Base) {
   'use strict';
-
-  // BBB: we need to hook pattern to classes which plone was using until now
-  var Plone = Base.extend({
-    name: 'plone',
-    init: function() {}
-  });
 
   // initialize only if we are in top frame
   if (window.parent === window) {
@@ -34,5 +54,5 @@ define([
       }
     });
   }
-  return Plone;
+
 });
